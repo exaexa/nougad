680eb592de03feb72ebd5afde402fba591ea226b
Mirek Kratochvil <exa.exa@gmail.com>
2021-07-24 22:04:02 +0200

    add extra params to nougad.benchmark

--- R/benchmark.R
+++ R/benchmark.R
@@ -4,6 +4,9 @@
 #'
 #' @export
 nougad.benchmark <- function(n=10240, k=30, d=40,
+  snw = 3, spw = 1, nw = 1,
+  snwf=function(sp)snw, spwf=function(sp)spw, nwf=function(sp)nw,
+  alpha=0.05, accel=1,
   n_pheno=20, pheno_positive_probability=0.2,
   plot.k=min(c(k,5)),
   plotf=plot) {
@@ -21,12 +24,12 @@ nougad.benchmark <- function(n=10240, k=30, d=40,
   emitted <- exprs %*% spectra
   received <- emitted + rnorm(length(emitted),sd=0.0005*sqrt(rowSums(emitted^2)))
   
-  res <- nougad(received, spectra, 3, 1, 5, alpha=0.05)$unmixed
+  t <- system.time(res <- nougad(received, spectra, snwf(spectra), spwf(spectra), nwf(spectra), alpha=alpha, accel=accel)$unmixed)
 
   trans <- function(x)asinh(x/100)
 
   par(mfrow=c(1,3))
   plotf(main=paste("Original data -",k,"markers in",d,"channels"), trans(exprs[,1:2]), pch='.', xlab='', ylab='')
   plotf(main="OLS", trans(t(lm(t(received)~t(spectra)+0)$coefficients)[,1:2]), pch='.', xlab='', ylab='')
-  plotf(main="nougad", trans(res[,1:2]), pch='.', xlab='', ylab='')
+  plotf(main=paste0("nougad (elapsed: ",round(t['elapsed'], digits=3),")"), trans(res[,1:2]), pch='.', xlab='', ylab='')
 }
