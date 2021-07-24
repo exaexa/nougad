864ba30f513d83cab6d142384240db1c1f8e3f64
Mirek Kratochvil <exa.exa@gmail.com>
2021-07-24 15:40:02 +0200

    add comp shaders to the autostyler

--- src/style.sh
+++ src/style.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0` #lol
-clang-format -style="{BasedOnStyle: Mozilla, PointerAlignment: Right}" -verbose -i `find -name '*.c' -or -name '*.cpp' -or -name  '*.h' -or -name '*.hpp' -or -name '*.cu' -or -name '*.cuh'`
+clang-format -style="{BasedOnStyle: Mozilla, PointerAlignment: Right}" -verbose -i `find -name '*.c' -or -name '*.cpp' -or -name  '*.h' -or -name '*.hpp' -or -name '*.cu' -or -name '*.cuh' -or -name '*.comp'`
