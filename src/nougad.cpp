5a90ecc309b91621f76b0f44968af5cdd1b47188
Mirek Kratochvil <exa.exa@gmail.com>
2021-07-24 15:39:17 +0200

    implement the vulkan version (bundles vuh)

---
+++ src/nougad.cpp
@@ -0,0 +1,207 @@
+
+#include <R.h>
+#include <R_ext/Rdynload.h>
+#include <Rmath.h>
+#include <cstdint>
+#include <iterator>
+#include <vector>
+#include <vuh/array.hpp>
+#include <vuh/vuh.h>
+
+#include "unmix.comph"
+
+const size_t batch_size = 10240;
+const size_t local_size = 64;
+
+/*
+ * fancy iterator for feeding in the pre-multiplied residual weights
+ */
+
+template<typename I>
+struct mult_iter
+{
+  using iterator_category = std::input_iterator_tag;
+  using difference_type = std::ptrdiff_t;
+  using value_type = typename I::value_type;
+  using reference = typename I::value_type;
+
+  I a, b;
+
+  mult_iter(I a, I b)
+    : a(a)
+    , b(b)
+  {}
+  mult_iter(I a)
+    : a(a)
+    , b(a)
+  {}
+
+  ptrdiff_t operator-(mult_iter right) const { return a - right.a; }
+
+  float operator*() const { return *a * *b; }
+
+  mult_iter &operator++()
+  {
+    ++a;
+    ++b;
+    return *this;
+  }
+
+  bool operator==(mult_iter other) const { return a == other.a; }
+  bool operator!=(mult_iter other) const { return a != other.a; }
+  bool operator<(mult_iter other) const { return a < other.a; }
+};
+
+/*
+ * transposed matrix view -- iterating on transposed(ptr,rows,cols) that is
+ * stored by columns outputs (or ingests) the numbers as if it was read by
+ * rows.
+ */
+
+template<typename T>
+struct transposed
+{
+
+  T *ptr;
+  const size_t a, b;
+
+  transposed(T *ptr, size_t a, size_t b)
+    : ptr(ptr)
+    , a(a)
+    , b(b)
+  {}
+
+  struct tr_iter
+  {
+    using iterator_category = std::input_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = T;
+    using reference = T &;
+
+    size_t off;
+    transposed base;
+    tr_iter(size_t off, transposed base)
+      : off(off)
+      , base(base)
+    {}
+
+    inline reference operator*() const
+    {
+      const size_t blk = off / base.b;
+      const size_t item = off % base.b;
+      return base.ptr[blk + base.a * item];
+    }
+
+    tr_iter &operator++()
+    {
+      ++off;
+      return *this;
+    }
+
+    ptrdiff_t operator-(tr_iter other) const { return off - other.off; }
+    bool operator==(tr_iter other) const { return off == other.off; }
+    bool operator!=(tr_iter other) const { return off != other.off; }
+    bool operator<(tr_iter other) const { return off < other.off; }
+  };
+
+  tr_iter begin() const { return tr_iter(0, *this); }
+  tr_iter end() const { return tr_iter(a * b, *this); }
+  tr_iter at(size_t ia, size_t ib) const { return tr_iter(ib + b * ia, *this); }
+};
+
+/*
+ * nougad entrypoint
+ */
+
+extern "C" void
+nougad_c(const int *np,
+         const int *dp,
+         const int *kp,
+         const int *itersp,
+         const float *alphap,
+         const float *accelp,
+         const float *s_dk,
+         const float *spw_dk,
+         const float *snw_dk,
+         const float *nw_k,
+         const float *y_dn,
+         float *x_kn,
+         float *r_dn)
+{
+  const size_t n = *np, d = *dp, k = *kp, iters = *itersp;
+  const float alpha = *alphap, accel = *accelp;
+
+  auto instance = vuh::Instance();
+  auto device = instance.devices().at(0); // TODO parametrize
+
+  vuh::Array<float> vu_s_kd(device, k * d), vu_spw_kd(device, k * d),
+    vu_snw_kd(device, k * d), vu_nw_k(device, k),
+    vu_y_nd(device, batch_size * d), vu_x_nk(device, batch_size * k),
+    vu_r_nd(device, batch_size * d);
+
+  vu_s_kd.fromHost(transposed(s_dk, d, k).begin(),
+                   transposed(s_dk, d, k).end());
+  vu_spw_kd.fromHost(
+    mult_iter(transposed(spw_dk, d, k).begin(), transposed(s_dk, d, k).begin()),
+    mult_iter(transposed(spw_dk, d, k).end()));
+  vu_snw_kd.fromHost(
+    mult_iter(transposed(snw_dk, d, k).begin(), transposed(s_dk, d, k).begin()),
+    mult_iter(transposed(snw_dk, d, k).end()));
+  vu_nw_k.fromHost(nw_k, nw_k + k);
+
+  using Specs = vuh::typelist<uint32_t /*n_blocks*/,
+                              uint32_t /* k */,
+                              uint32_t /* d */,
+                              uint32_t /* iterations */>;
+  struct Params
+  {
+    uint32_t n_cells;
+    float alpha;
+    float accel;
+  };
+
+  auto program =
+    vuh::Program<Specs, Params>(device, sizeof(spirv_unmix), spirv_unmix);
+
+  for (size_t batch_off = 0; batch_off < n; batch_off += batch_size) {
+    size_t local_n = std::min(batch_size, n - batch_off);
+
+    auto xv = transposed(x_kn + k * batch_off, k, local_n);
+    auto yv = transposed(y_dn + d * batch_off, d, local_n);
+    auto rv = transposed(r_dn + d * batch_off, d, local_n);
+
+    vu_x_nk.fromHost(xv.begin(), xv.end());
+    vu_y_nd.fromHost(yv.begin(), yv.end());
+
+    program.grid(vuh::div_up(local_n, local_size))
+      .spec(local_size, k, d, iters)({ uint32_t(local_n), alpha, accel },
+                                     vu_s_kd,
+                                     vu_spw_kd,
+                                     vu_snw_kd,
+                                     vu_nw_k,
+                                     vu_y_nd,
+                                     vu_x_nk,
+                                     vu_r_nd);
+
+    vu_x_nk.toHost(xv.begin());
+    vu_r_nd.toHost(rv.begin());
+  }
+}
+
+/*
+ * R API connector
+ */
+
+extern "C"
+{
+  static const R_CMethodDef cMethods[] = {
+    { "nougad_c", (DL_FUNC)&nougad_c, 13 },
+    { NULL, NULL, 0 }
+  };
+
+  void R_init_nougad(DllInfo *info)
+  {
+    R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, FALSE);
+  }
+} // extern "C"
---
+++ src/vuh/include/vuh/vuh.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "device.h"
+#include "error.h"
+#include "instance.h"
+#include "program.hpp"
+#include "utils.h"
